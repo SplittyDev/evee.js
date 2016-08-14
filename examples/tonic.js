2b93b61f15efbaa306f23287cfe04b43c508d5a8
SplittyDev <splittydev@gmail.com>
2016-08-14 19:30:49 +0200

    Update README and tonic example

--- examples/tonic.js
+++ examples/tonic.js
@@ -1,9 +1,11 @@
-// HINT: Switch to Node 6 if the example does not work :)
+// evee.js uses ECMAScript6 features
+// Please switch to Node 6 if the example does not work
 
 'use strict';
 var Evee = require('evee'),
     evee = new Evee();
 
 evee.on('say', e => console.log(e.data));
-console.log('wait for it...');
+
+// delay for 1200ms and then fire the event
 setTimeout(() => evee.emit('say', 'hello from evee.js!'), 1200);
