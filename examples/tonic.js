81aa7e01e527556fec9721d23ace3181230f0e38
SplittyDev <splittydev@gmail.com>
2016-08-14 05:57:19 +0200

    Update example

--- examples/tonic.js
+++ examples/tonic.js
@@ -1,6 +1,9 @@
+// HINT: Switch to Node 6 if the example does not work :)
+
 'use strict';
-var Evee = require('evee.compatible'),
+var Evee = require('evee'),
     evee = new Evee();
 
-evee.on('say', e => window.alert(e.data));
-evee.emit('say', 'hello from evee.js!');
+evee.on('say', e => console.log(e.data));
+console.log('wait for it...');
+setTimeout(() => evee.emit('say', 'hello from evee.js!'), 1200);
